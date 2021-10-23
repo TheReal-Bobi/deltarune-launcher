e89ac0b4ba506078381126e794750a65a8259c45
TheReal-Bobi <80277857+TheReal-Bobi@users.noreply.github.com>
2021-10-23 12:26:49 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Deltarune_Chapter_1_and_2_Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Español = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Cancelar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Español
+            // 
+            this.Español.Location = new System.Drawing.Point(5, 76);
+            this.Español.Name = "Español";
+            this.Español.Size = new System.Drawing.Size(97, 23);
+            this.Español.TabIndex = 0;
+            this.Español.Text = "Español/Spanish";
+            this.Español.UseVisualStyleBackColor = true;
+            this.Español.Click += new System.EventHandler(this.Español_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Original";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(276, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Open DELTARUNE\'s original version or spanish version?";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(352, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "¿Quieres jugar la versión original de DELTARUNE o la versión española?";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Cancelar
+            // 
+            this.Cancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Cancelar.Location = new System.Drawing.Point(323, 76);
+            this.Cancelar.Name = "Cancelar";
+            this.Cancelar.Size = new System.Drawing.Size(97, 23);
+            this.Cancelar.TabIndex = 4;
+            this.Cancelar.Text = "Cancelar/Abort";
+            this.Cancelar.UseVisualStyleBackColor = true;
+            this.Cancelar.Click += new System.EventHandler(this.Cancelar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(2, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(346, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "La versión española puede congelarse o crashear en ciertos momentos,";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(2, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(247, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "consulta el archivo léeme.txt para más información.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.CancelButton = this.Cancelar;
+            this.ClientSize = new System.Drawing.Size(424, 100);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Cancelar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Español);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(440, 139);
+            this.MaximumSize = new System.Drawing.Size(440, 139);
+            this.MinimumSize = new System.Drawing.Size(440, 139);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Deltarune Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Español;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Cancelar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Deltarune_Chapter_1_and_2_Launcher
+{
+
+    public partial class Form1 : Form
+    {
+        private const string datafileenglish = "El archivo data.win no existe en la carpeta de la versión original. Por favor, reinstala este programa. /data.win file doesn't exist on the original game's folder. Please reinstall this program.";
+        private const string exefileenglish = "El archivo DELTARUNE.exe no existe en la carpeta de la versión original. Por favor, reinstala este programa. /DELTARUNE.exe file doesn't exist on the original game's folder. Please reinstall this program.";
+        private const string datafilespanish = "El archivo data.win no existe en la carpeta de la versión original. Por favor, reinstala este programa.";
+        private const string exefilespanish = "El archivo DELTARUNE.exe no existe en la carpeta de la versión original. Por favor, reinstala este programa.";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Español_Click(object sender, EventArgs e)
+        {
+            string rutaeswin = @"C:\Program Files (x86)\Deltarune\es\data.win";
+            if (File.Exists(rutaeswin))
+            {
+                string rutaesexe = @"C:\Program Files (x86)\Deltarune\es\DELTARUNE.exe";
+                if (File.Exists(rutaesexe))
+                {
+                    System.Diagnostics.Process.Start(@"C:\Program Files (x86)\Deltarune\es\DELTARUNE.exe");
+                    Application.Exit();
+                }
+                else
+                {
+                    {
+                        string exeesfilemissing = exefilespanish;
+                        MessageBox.Show(exefilespanish);
+                        Application.Exit();
+                    }
+                }
+            }
+            else
+            {
+                {
+                    string dataesfilemissing = datafilespanish;
+                    MessageBox.Show(dataesfilemissing);
+                    Application.Exit();
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string rutaenwin = @"C:\Program Files (x86)\Deltarune\en\data.win";
+            if (File.Exists(rutaenwin))
+            {
+                string rutaenexe = @"C:\Program Files (x86)\Deltarune\en\DELTARUNE.exe";
+                if (File.Exists(rutaenexe))
+                {
+                    System.Diagnostics.Process.Start(@"C:\Program Files (x86)\Deltarune\en\DELTARUNE.exe");
+                    Application.Exit();
+                }
+                else
+                {
+                    {
+                        string exeenfilemissing = exefileenglish;
+                        MessageBox.Show(exefileenglish);
+                        Application.Exit();
+                    }
+                }
+            }
+            else
+            {
+                {
+                    string dataenfilemissing = datafileenglish;
+                    MessageBox.Show(dataenfilemissing);
+                    Application.Exit();
+                }
+            }
+            
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Cancelar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
